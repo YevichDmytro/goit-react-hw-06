434ed4a6e57ecc5ed971a248274963ab5d8a8769
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-07-30 11:45:48 +0300

    upd-1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,8 +3,93 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid';
 import { Button } from '@mui/material';
 import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contactsSlice';
 
-const ContactForm = ({ onAdd }) => {
+// const ContactForm = ({ onAdd }) => {
+//   const FeedbackSchema = Yup.object().shape({
+//     name: Yup.string()
+//       .min(2, 'Too Short!')
+//       .max(50, 'Too Long!')
+//       .required('Required'),
+//     number: Yup.string()
+//       .min(3, 'Too Short!')
+//       .max(50, 'Too Long!')
+//       .required('Required'),
+//   });
+
+//   const handleSubmit = (values, actions) => {
+//     const userName = values.name.trim();
+//     const userNumber = values.number.trim();
+
+//     onAdd({ name: userName, number: userNumber });
+//     actions.resetForm();
+//   };
+
+//   const id = nanoid();
+
+//   return (
+//     <Formik
+//       initialValues={{ name: '', number: '' }}
+//       onSubmit={handleSubmit}
+//       validationSchema={FeedbackSchema}
+//     >
+//       <Form className={style.form}>
+//         <div className={style.wrap}>
+//           <label htmlFor={`name-${id}`}>Name</label>
+//           <Field
+//             type='text'
+//             name='name'
+//             id={`name-${id}`}
+//             className={style.field}
+//             autoComplete='on'
+//             placeholder='Jack Robinson'
+//           />
+//           <ErrorMessage
+//             name='name'
+//             component='span'
+//             className={style.errorText}
+//           />
+//         </div>
+
+//         <div className={style.wrap}>
+//           <label htmlFor={`number-${id}`}>Number</label>
+//           <Field
+//             type='text'
+//             name='number'
+//             id={`number-${id}`}
+//             className={style.field}
+//             placeholder='000-00-00'
+//             autoComplete='on'
+//           />
+//           <ErrorMessage
+//             name='number'
+//             component='span'
+//             className={style.errorText}
+//           />
+//         </div>
+
+//         <Button variant='contained' color='success' type='submit'>
+//           Add contact
+//         </Button>
+//       </Form>
+//     </Formik>
+//   );
+// };
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    const userName = values.name.trim();
+    const userNumber = values.number.trim();
+
+    dispatch(addContact({ name: userName, number: userNumber }));
+
+    actions.resetForm();
+  };
+
+  const id = nanoid();
   const FeedbackSchema = Yup.object().shape({
     name: Yup.string()
       .min(2, 'Too Short!')
@@ -15,16 +100,6 @@ const ContactForm = ({ onAdd }) => {
       .max(50, 'Too Long!')
       .required('Required'),
   });
-
-  const handleSubmit = (values, actions) => {
-    const userName = values.name.trim();
-    const userNumber = values.number.trim();
-
-    onAdd({ name: userName, number: userNumber });
-    actions.resetForm();
-  };
-
-  const id = nanoid();
 
   return (
     <Formik
